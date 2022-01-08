1d104cbe74d2fbb5fc223427c9401c0ec3c90377
BootyAss <panda_canniball@mail.ru>
2022-01-08 21:02:39 +0300

    kruskals maze implemented

--- src/algs.js
+++ src/algs.js
@@ -1,15 +1,99 @@
-var Algs = {
+var Types = {
+    empty: 'empty',
+    wall: 'wall',
+    path: 'path',
+    variant: 'variant',
+}
+
+
+var Colors = Types;
+
+
+var Points = {
+    start: 'start',
+    end: 'end'
+}
+
+var Logos = Points;
+
+
+var Lib = {
     rand: (len) => { return _rand(len); },
-    Kruskal: (x, y) => { return _Kraskal(len); }
-};
+}
 
 var _rand = (len) => {
     return Math.round(Math.random() * len);
 };
 
-var _Kraskal = (x, y) => {
-    let walls = new Set();
-    return 1;
+
+var Algs = {
+    Astar: (x, y, start, end, walls) => { return _Astar(x, y, start, end, walls); },
+    Djikstra: () => { },
+    BFS: () => { },
 };
 
-export { Algs };
+var _Astar = (x, y, start, end, walls) => {
+    return [0, x, 2 * x, 2 * x + 1, 2 * x + 2, 2 * x + 3, 2 * x + 4];
+}
+
+var Mazes = {
+    Kruskal: (x, y) => { return _Kruskal(x, y); },
+}
+
+var getNeighbours = (idx, A, x, y) => {
+    let indexes = [idx - 2 * x, idx - 2, idx + 2, idx + 2 * x];
+    let checks = [idx > 2 * x, idx % x > 1, idx % x < x - 2, idx < x * (y - 1)];
+    let neighbours = []
+
+    for (let i = 0; i < 4; i++) {
+        if (checks[i] && !A.has(indexes[i]))
+            neighbours.push(indexes[i]);
+    }
+    return neighbours;
+}
+
+var _Kruskal = (x, y) => {
+    let walls = new Set();
+    let sets = new Object();
+
+    for (let i = 0; i < y; i++)
+        for (let j = 0; j < x; j++) {
+            let idx = i * x + j;
+            if (i % 2 || j % 2)
+                walls.add(idx);
+            else
+                sets[idx] = new Set([idx]);
+        }
+
+    let setsKeys = Object.keys(sets);
+    while (setsKeys.length > 1) {
+        let keyA = setsKeys[_rand(setsKeys.length - 1)];
+        let A = sets[keyA];
+
+        let parent = null;
+        let neighbours = null;
+        for (let p of A) {
+            parent = p;
+            neighbours = getNeighbours(parent, A, x, y);
+            if (neighbours.length)
+                break;
+        }
+        if (!neighbours.length) 
+            break;
+
+        let neighbour = neighbours[_rand(neighbours.length - 1)];
+        let keyB = setsKeys.find(key => sets[key].has(neighbour));
+        let B = sets[keyB];
+        for (let b of B)
+            sets[keyA].add(b);
+
+        walls.delete(neighbour - (neighbour - parent) / 2);
+        const index = setsKeys.indexOf(keyB);
+        if (index > -1)
+          setsKeys.splice(index, 1);
+    }
+
+    return walls;
+};
+
+export { Types, Colors, Points, Logos, Lib, Mazes, Algs };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import { Algs } from './algs.js';
+import { Types, Colors, Points, Logos, Lib, Mazes, Algs } from './algs.js';
 
 
 var [x, y] = [null, null];
-var walls = null;
+var walls = new Set();
 var cellSize = 22;
+var start = null;
+var end = null;
+var current = {
+    PointType: null,
+    Algorithm: Algs.Astar
+}
 
 
 var animateCell = (cell) => {
@@ -16,8 +22,10 @@ var changeCellType = (cell, type, id = null) => {
     cell.className = 'cell ' + type;
 
     let size = cellSize;
-    if (type === 'empty')
+    if (type === 'empty') {
+        cell.style.border
         size -= 2
+    }
     cell.style.padding = size / 2 + 'px';
     animateCell(cell);
 }
@@ -30,14 +38,102 @@ var createCell = (type, id) => {
     if (type === 'empty')
         size -= 2
     cell.style.padding = size / 2 + 'px';
+    // cell.textContent = id;
     return cell;
 }
 
+
+var clearTable = () => {
+    for (let i = 0; i < x * y; i++) {
+        walls = new Set();
+        changeCellType(null, 'empty', i);
+    }
+}
+
+
+var navClickHandler = (e) => {
+    let foo = null;
+    switch (e.target.className) {
+        case 'maze':
+            for (let f in Mazes)
+                if (f == e.target.textContent)
+                    foo = f;
+            if (!foo)
+                break;
+            
+            clearTable();
+            walls = Mazes[foo](x, y);
+            for (let wall of walls)
+                changeCellType(null, 'wall', wall);
+            break;
+        
+        case 'alg':
+            for (let f in Algs)
+                if (f == e.target.textContent)
+                    foo = f;
+            if (!foo)
+                break;
+            
+            current.Algorithm = Algs[foo];
+            let path = Algs[foo](x, y, start, end, walls);
+            for (let p of path)
+                changeCellType(null, 'path', p);
+        
+        default:
+            break;
+    }
+}
+
+var showError = (e) => {
+    console.log(e)
+}
+
+var Visualize = () => {
+    if (!start || !end)
+        return showError('no start/end');
+    
+    let path = Algs.Astar(x, y, start, end, walls);
+    for (let p of path)
+        changeCellType(null, 'path', p);
+}
+
 var setup = () => {
+    let blocks = {
+        mazes: Object.keys(Mazes),
+        types: Object.keys(Types),
+        colors: Object.keys(Colors),
+        points: Object.keys(Points),
+        logos: Object.keys(Logos),
+        algs: Object.keys(Algs),
+    }
+
+    for (let [block, labels] of Object.entries(blocks)) {
+        let navBlock = document.getElementById(block);
+        for (let label of labels) {
+            let navLabel = document.createElement('div');
+            navLabel.className = block.slice(0, -1);
+            if (!['colors', 'logos'].includes(block)) 
+                navLabel.textContent = label;
+            else
+                navLabel.className = navLabel.className + ' ' + label;
+            if (['logos', 'algs', 'mazes'].includes(block))
+                navLabel.style.cursor = 'pointer';
+                
+            navLabel.onclick = navClickHandler;
+            navBlock.appendChild(navLabel);
+        }
+    }
+
+
     let content = document.getElementById('cont');
     let tableContainer = document.getElementById('tabCon');
-    x = Math.floor((content.clientWidth - 100) / cellSize);
-    y = Math.floor((content.clientHeight - 250) / cellSize);
+    if (!x || !y) {
+        x = Math.floor((content.clientWidth * 0.95) / cellSize);
+        y = Math.floor((content.clientHeight * 0.75) / cellSize);
+        x = !(x % 2) ? x - 1 : x;
+        y = !(y % 2) ? y - 1 : y;
+    }
+    console.log(x, y)
     let table = document.createElement('div');
     tableContainer.style.width = x * cellSize + 'px';
     tableContainer.style.height = y * cellSize + 'px';
@@ -49,13 +145,16 @@ var setup = () => {
     for (let i = 0; i < y; i++) {
         let row = document.createElement('div');
         row.className = 'row';
-        row.id = i;
         for (let j = 0; j < x; j++) {
             let cell = createCell('empty', i * x + j);
             row.appendChild(cell);
         }
         table.appendChild(row);
     }
+
+    let visualize = document.getElementById('button');
+    visualize.onclick = Visualize;
+
 }
 
 
